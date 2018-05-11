36b96c8f41371f0cad4ddbea882e75d74fa4fe99
leandrocarra <leandrocarra@gmail.com>
2018-05-10 21:57:04 -0300

    add list

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name='About' small='who we are'></PageHeader>
+    <h2>Lorem ipsum dolor sit amet</h2>
+
+  </div>
+)
