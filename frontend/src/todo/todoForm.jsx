d9a5dae54cecc59183bd75d38c82c61bffdb7809
leandrocarra <leandrocarra@gmail.com>
2018-05-16 00:24:44 -0300

    add api

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,11 +6,22 @@ export default props => (
   <div role='form' className='todoForm'>
 
     <Grid cols='12 9 10'>
-      <input id='description' className='form-control' placeholder='test'></input>
+      <input
+        id='description'
+        className='form-control'
+        placeholder='test'
+        value={props.description}
+        onChange={props.handleChange}
+        ></input>
     </Grid>
 
     <Grid cols='12 3 2'>
-      <IconButton style='primary' icon='plus'></IconButton>
+      <IconButton
+        style='primary'
+        icon='plus'
+        onClick={props.handleAdd}
+        >
+        </IconButton>
     </Grid>
 
   </div>
